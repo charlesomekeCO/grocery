85b9d570476c6ef1c3a29b58b61015b360cf3771
Charles Omeke <168457171+charlesomekeCO@users.noreply.github.com>
2024-11-06 13:52:31 +0100

    Add files via upload

---
+++ beginners.js
@@ -0,0 +1,17 @@
+function greet(name) {
+    console.log("Good morning " +  name)
+}
+
+greet("Hannah");
+
+const speak=function(){
+    console.log("How are you")
+}
+
+speak()
+
+const move=function(name, firstname) {
+    console.log("Who is this " + name )
+}
+
+move("Guy","book")
